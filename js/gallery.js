f14123485ab96fe93a48472b677f03d3b0020986
vladblkvs <gremlinux@gmail.com>
2019-07-02 23:23:23 +0300

    Реализует загрузку данных с сервера

--- js/gallery.js
+++ js/gallery.js
@@ -2,26 +2,25 @@
 
 (function () {
   window.gallery = {};
-
-  // Поиск шаблона
-  var picture = document.querySelector('#picture').content.querySelector('.picture');
+  window.gallery.renderAllCards = function (dataCards) {
+    // Вставляет циклом карточки в место, определённое переменной pictureListElement
+    var pictureListElement = document.querySelector('.pictures');
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < dataCards.length; i++) {
+      fragment.appendChild(renderCard(dataCards[i]));
+    }
+    pictureListElement.appendChild(fragment);
+  };
 
   // Заполняет содержимое карточки
   var renderCard = function (card) {
+    var picture = document.querySelector('#picture').content.querySelector('.picture');
     var cardElement = picture.cloneNode(true);
 
     cardElement.querySelector('.picture__img').src = card.url;
     cardElement.querySelector('.picture__likes').textContent = card.likes;
-    cardElement.querySelector('.picture__comments').textContent = window.utility.getNumberInRange(window.data.MIN_COMMENTS_COUNT, window.data.MAX_COMMENTS_COUNT);
+    cardElement.querySelector('.picture__comments').textContent = card.comments.length;
 
     return cardElement;
   };
-
-  // Вставляет циклом карточки в место, определённое переменной pictureListElement
-  var pictureListElement = document.querySelector('.pictures');
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < window.data.CARD_AMOUNT; i++) {
-    fragment.appendChild(renderCard(window.data.renderCards()[i]));
-  }
-  pictureListElement.appendChild(fragment);
 })();
