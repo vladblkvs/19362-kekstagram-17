7c1ee98529e34819b22d6d47ab87b994006478ce
vladblkvs <gremlinux@gmail.com>
2019-07-03 23:05:28 +0300

    Показывает кнопки фильтрации фотографий после загрузки карточек

--- js/data.js
+++ js/data.js
@@ -5,6 +5,9 @@
   var onError = function () {};
   var onSuccess = function (data) {
     window.gallery.renderAllCards(data);
+
+    var imageFilters = document.querySelector('.img-filters');
+    imageFilters.classList.remove('img-filters--inactive');
   };
   window.load('https://js.dump.academy/kekstagram/data', onSuccess, onError);
 })();
