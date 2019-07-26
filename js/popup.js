61f07dccc84df9703be381ec4a28ff50ba86887c
vladblkvs <vladblkvs@gmail.com>
2019-07-26 12:37:20 +0300

    Реализует загрузку изображения с компьютера для последующего наложения цветовых фильтров

--- js/popup.js
+++ js/popup.js
@@ -161,4 +161,26 @@
     hashTagField.setCustomValidity(gethashtagsArrMistakes(hashTags).join(''));
   };
   hashTagField.addEventListener('change', onHashtagInput);
+
+  var fileChooser = document.querySelector('.img-upload__input');
+  var preview = document.querySelector('.img-upload__preview img');
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
 })();
